475c14a38f1e1f6af57bb734defb74f6fead2269
Vivek Manoharan <vivek.m@vuedata.in>
2022-10-13 20:42:15 +0530

    string push

---
+++ basicstring.py
@@ -0,0 +1,17 @@
+x = "This is strong"
+y = 'This is also string'
+z = 'this is string \'with esc \' char '
+a = """
+Multi 
+Line
+String 
+"""
+b '''
+This is also multy line string
+'''
+
+print(x)
+print(y)
+print(z)
+print(a)
+print(b)
