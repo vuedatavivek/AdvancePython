f6703ff9c17968ac7305aed30f02bc909fee9521
Vivek Manoharan <vivek.m@vuedata.in>
2022-10-13 21:59:33 +0530

    13-10-2022 1

--- basicstring.py
+++ basicstring.py
@@ -1,3 +1,6 @@
+from datetime import date, datetime
+
+
 x = "This is strong"
 y = 'This is also string'
 z = 'this is string \'with esc \' char '
@@ -6,8 +9,10 @@ Multi
 Line
 String 
 """
-b '''
-This is also multy line string
+b = '''
+This is also 
+multy line 
+string
 '''
 
 print(x)
@@ -15,3 +20,9 @@ print(y)
 print(z)
 print(a)
 print(b)
+print('len',len(x))
+print('str', str(2))
+print(x.find('is'))
+print("x is "+ x + " y is" + y)
+print ("x is %s and y is %s" %(x,y))
+print ("x is {} and {} is y".format(x,y))
